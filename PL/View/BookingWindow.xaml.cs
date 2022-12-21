6aad564df63f7266b1e04d2e0c8311b84620971f
Oleg Neveykin <neolbullet@gmail.com>
2022-12-21 19:46:33 +0300

    Initial Commit

---
+++ PL/View/BookingWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HotelFarAwayHome.View
+{
+    /// <summary>
+    /// Логика взаимодействия для BookingWindow.xaml
+    /// </summary>
+    public partial class BookingWindow : UserControl
+    {
+        public BookingWindow(/*ApplicationViewModel appViewModel*/)
+        {
+            //InitializeComponent();
+            //DataContext = appViewModel;
+        }
+    }
+}
